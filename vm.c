374930742f39124b5f220e3ddd11b2d424b46126
Mark Fosseddy <markfosseddy@gmail.com>
2022-08-10 19:38:36 +0500

    add jmp and jmp_if, clean up

--- vm.c
+++ vm.c
@@ -1,12 +1,22 @@
+// @TODO(art): Load program from file
+// @TODO(art): Proper errors
+
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
 
 #define VM_STACK_CAP 1024
+#define VM_PROGRAM_CAP 1024
 
 enum inst_kind {
   INST_KIND_PUSH,
-  INST_KIND_ADD
+  INST_KIND_ADD,
+  INST_KIND_SUB,
+  INST_KIND_MUL,
+  INST_KIND_DIV,
+  INST_KIND_JMP,
+  INST_KIND_JMP_IF,
+  INST_KIND_HALT,
 };
 
 struct inst {
@@ -16,24 +26,73 @@ struct inst {
 
 struct vm {
   int stack[VM_STACK_CAP];
-  int stack_size;
+  size_t stack_size;
+
+  struct inst program[VM_PROGRAM_CAP];
+  size_t program_size;
+  size_t ip;
+
+  int halt;
 };
 
 void vm_exec_inst(struct vm *vm, struct inst *inst)
 {
+  assert(vm->ip >= 0 && vm->ip <= vm->program_size);
+
   switch (inst->kind) {
-    case INST_KIND_PUSH:
-      assert(vm->stack_size < VM_STACK_CAP);
-      vm->stack[vm->stack_size++] = inst->value;
-      break;
+  case INST_KIND_PUSH:
+    assert(vm->stack_size < VM_STACK_CAP);
+    vm->stack[vm->stack_size++] = inst->value;
+    vm->ip++;
+    break;
 
-    case INST_KIND_ADD:
-      assert(vm->stack_size > 1);
-      vm->stack[vm->stack_size - 2] += vm->stack[vm->stack_size - 1];
-      vm->stack_size -= 1;
-      break;
+  case INST_KIND_ADD:
+    assert(vm->stack_size > 1);
+    vm->stack[vm->stack_size - 2] += vm->stack[vm->stack_size - 1];
+    vm->stack_size -= 1;
+    vm->ip++;
+    break;
 
-    default: assert(0 && "Unreachable");
+  case INST_KIND_SUB:
+    assert(vm->stack_size > 1);
+    vm->stack[vm->stack_size - 2] -= vm->stack[vm->stack_size - 1];
+    vm->stack_size -= 1;
+    vm->ip++;
+    break;
+
+  case INST_KIND_MUL:
+    assert(vm->stack_size > 1);
+    vm->stack[vm->stack_size - 2] *= vm->stack[vm->stack_size - 1];
+    vm->stack_size -= 1;
+    vm->ip++;
+    break;
+
+  case INST_KIND_DIV:
+    assert(vm->stack_size > 1);
+    assert(vm->stack[vm->stack_size - 1] != 0);
+    vm->stack[vm->stack_size - 2] /= vm->stack[vm->stack_size - 1];
+    vm->stack_size -= 1;
+    vm->ip++;
+    break;
+
+  case INST_KIND_JMP:
+    vm->ip = inst->value;
+    break;
+
+  case INST_KIND_JMP_IF:
+    if (vm->stack[vm->stack_size - 1] == 1) {
+      vm->ip = inst->value;
+    } else {
+      vm->ip++;
+    }
+    vm->stack_size -= 1;
+    break;
+
+  case INST_KIND_HALT:
+    vm->halt = 1;
+    break;
+
+  default: assert(0 && "Unreachable");
   }
 }
 
@@ -46,12 +105,21 @@ void vm_dump_stack(struct vm *vm)
 }
 
 static struct inst program[] = {
+  { .kind = INST_KIND_JMP, .value = 2 },
   { .kind = INST_KIND_PUSH, .value = 5 },
+
   { .kind = INST_KIND_PUSH, .value = 3 },
-  { .kind = INST_KIND_ADD },
-  { .kind = INST_KIND_PUSH, .value = 2 },
-  { .kind = INST_KIND_ADD },
-  { .kind = INST_KIND_ADD }
+  { .kind = INST_KIND_PUSH, .value = 9 },
+  { .kind = INST_KIND_MUL },
+
+  { .kind = INST_KIND_PUSH, .value = 26 },
+  { .kind = INST_KIND_SUB },
+
+  { .kind = INST_KIND_JMP_IF, .value = 9 },
+  { .kind = INST_KIND_PUSH, .value = 69 },
+  { .kind = INST_KIND_PUSH, .value = 420 },
+
+  { .kind = INST_KIND_HALT },
 };
 
 int main(void)
@@ -60,7 +128,12 @@ int main(void)
 
   int sz = sizeof(program) / sizeof(program[0]);
   for (size_t i = 0; i < sz; ++i) {
-    vm_exec_inst(&vm, &program[i]);
+    assert(i < VM_PROGRAM_CAP);
+    vm.program[vm.program_size++] = program[i];
+  }
+
+  while (!vm.halt) {
+    vm_exec_inst(&vm, &vm.program[vm.ip]);
     vm_dump_stack(&vm);
   }
 
