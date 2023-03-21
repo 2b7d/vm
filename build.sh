4af176f92d920dfee52f844ea03cc0de64540855
fosseddy <markfosseddy@gmail.com>
2023-03-21 19:01:34 +0500

    build

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ set -e
 name="$1"
 flags="-Werror=declaration-after-statement -Wall -Wextra -pedantic -std=c99 -g"
 libs=""
+files=""
+
+if [ "$name" = "asm" ] || [ "$name" = "preproc" ]; then
+    libs="-I/home/fosseddy/programming/c-sandbox/mem -L/home/fosseddy/programming/c-sandbox/mem -lmem"
+    files="util.c"
+fi
 
 if [ "$2" = "prod" ]; then
     flags=${flags/-g/-O2}
 fi
 
-if [ "$name" = "asm" ] || [ "$name" = "preproc" ]; then
-    libs="-I/home/fosseddy/programming/c-sandbox/mem -L/home/fosseddy/programming/c-sandbox/mem -lmem"
-fi
-
-gcc $flags -o $name $name.c $libs
+gcc $flags -o $name $name.c $files $libs
