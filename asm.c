05c6381e9600b1d7c16b0cbbbf50f2d5a05e7a9b
fosseddy <markfosseddy@gmail.com>
2023-03-25 21:11:28 +0500

    context free gramma start

--- asm.c
+++ asm.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdint.h>
 
 #include "vm.h"
 #include "util.h"
 #include "mem.h" // lib
 
 struct scanner {
-    char *src;
     char *cur;
+    char *start;
+    char *src;
 };
 
 enum token_kind {
-    TOKEN_OPCODE,
-    TOKEN_IMMEDIATE,
-    TOKEN_LABEL
+    TOK_NUM,
+    TOK_STR,
+    TOK_OPCODE,
+    TOK_KWD,
+    TOK_SYMBOL,
+    TOK_COMMA,
+    TOK_COLON,
+    TOK_EOF
 };
 
 struct token {
-    enum vm_opcode opcode;
     enum token_kind kind;
-    uint8_t is_word;
-    uint16_t value;
-    char *lex;
-    size_t lexlen;
+    char *start;
+    size_t len;
+    int value;
 };
 
 struct token_array {
@@ -37,16 +37,23 @@ struct token_array {
     struct token *buf;
 };
 
-struct label {
-    uint16_t addr;
-    char *start;
-    size_t len;
+enum kwd_kind {
+    KWD_DB,
+    KWD_EXTERN,
+    KWD_GLOBAL,
+    KWD_SECTION,
+    KWD_TEXT,
+    KWD_DATA,
+    KWD_COUNT
 };
 
-struct label_array {
-    size_t size;
-    size_t cap;
-    struct label *buf;
+char *kwds_str[KWD_COUNT] = {
+    [KWD_DB]      = "db",
+    [KWD_EXTERN]  = "extern",
+    [KWD_GLOBAL]  = "global",
+    [KWD_SECTION] = "section",
+    [KWD_TEXT]    = "text",
+    [KWD_DATA]    = "data"
 };
 
 char *opcodes_str[OP_COUNT] = {
@@ -86,284 +93,16 @@ char *opcodes_str[OP_COUNT] = {
     [OP_RET]      = "ret"
 };
 
-static size_t addr_offset = 0;
-
-void scan(struct scanner *s, struct token_array *tokens,
-          struct label_array *labels)
+int ischar(char c)
 {
-    while (*s->cur != '\0') {
-        char *start = s->cur;
-
-        switch (*s->cur) {
-        case ' ':
-        case '\t':
-        case '\r':
-        case '\n':
-            s->cur++;
-            break;
-
-        case ';':
-            while (*s->cur != '\n' && *s->cur != '\0') {
-                s->cur++;
-            }
-            break;
-
-        case '"':
-            s->cur++;
-            while (*s->cur != '"' && *s->cur != '\0') {
-                s->cur++;
-            }
-
-            if (*s->cur == '\0') {
-                printf("unterminated string %.*s...\n", (int) (s->cur - start),
-                                                        start);
-                exit(1);
-            }
-
-            for (int i = 1; i < s->cur - start; ++i) {
-                struct token *t;
-                char val = start[i];
-
-                memgrow(tokens, sizeof(struct token));
-                t = tokens->buf + tokens->size;
-                tokens->size++;
-
-                t->kind = TOKEN_IMMEDIATE;
-                t->value = val;
-                t->is_word = 0;
-                addr_offset++;
-            }
-
-            s->cur++;
-            break;
-
-        default:
-            if (isalpha(*s->cur) != 0 || *s->cur == '_') {
-                struct token *t;
-                size_t lexlen;
-
-                while (isalnum(*s->cur) != 0 || *s->cur == '_') {
-                    s->cur++;
-                }
-
-                lexlen = s->cur - start;
-
-                if (*s->cur == ':') {
-                    struct label *l;
-
-                    for (size_t i = 0; i < labels->size; ++i) {
-                        l = labels->buf + i;
-
-                        if (lexlen == l->len &&
-                                memcmp(start, l->start, l->len) == 0) {
-                            printf("label %.*s already declared\n",
-                                    (int) l->len, l->start);
-                            exit(1);
-                        }
-                    }
-
-                    memgrow(labels, sizeof(struct label));
-                    l = labels->buf + labels->size;
-                    labels->size++;
-
-                    l->addr = addr_offset;
-                    l->start = start;
-                    l->len = lexlen;
-                    s->cur++;
-                    break;
-                }
-
-                memgrow(tokens, sizeof(struct token));
-                t = tokens->buf + tokens->size;
-                tokens->size++;
-
-                t->kind = TOKEN_LABEL;
-                t->lex = start;
-                t->lexlen = lexlen;
-
-                for (size_t i = 0; i < OP_COUNT; ++i) {
-                    char *op = opcodes_str[i];
-
-                    if (start[lexlen - 1] == '8') {
-                        if (lexlen-1 == strlen(op) &&
-                                memcmp(start, op, lexlen-1) == 0) {
-                            t->kind = TOKEN_OPCODE;
-                            t->opcode = i;
-                            t->is_word = 0;
-                            break;
-                        }
-                    }
-
-                    if (lexlen == strlen(op) &&
-                            memcmp(start, op, lexlen) == 0) {
-                        t->kind = TOKEN_OPCODE;
-                        t->opcode = i;
-                        t->is_word = 1;
-                        break;
-                    }
-                }
-                if (t->kind == TOKEN_LABEL) {
-                    addr_offset += 2;
-                } else {
-                    addr_offset++;
-                }
-                break;
-            }
-
-            if (isdigit(*s->cur) != 0) {
-                struct token *t;
-                char valbuf[6];
-                size_t lexlen, val;
-
-                memgrow(tokens, sizeof(struct token));
-                t = tokens->buf + tokens->size;
-                tokens->size++;
-
-                while (isdigit(*s->cur) != 0) {
-                    s->cur++;
-                }
-
-                lexlen = s->cur - start;
-
-                if (lexlen >= sizeof(valbuf))  {
-                    printf("value %.*s is greater than 16bit\n", (int) lexlen,
-                                                                 start);
-                    exit(1);
-                }
-
-                memset(valbuf, 0, sizeof(valbuf));
-                memcpy(valbuf, start, lexlen);
-
-                val = atoi(valbuf);
-                if (val >= 1 << 16) {
-                    printf("value %lu is greater than 16bit\n", val);
-                    exit(1);
-                }
-
-                t->kind = TOKEN_IMMEDIATE;
-                t->value = val;
-                t->is_word = 0;
-
-                if (tokens->size > 0) {
-                    struct token *prev = tokens->buf + tokens->size - 2;
-
-                    if (prev->kind == TOKEN_OPCODE) {
-                        t->is_word = prev->is_word;
-                    }
-                }
-
-                if (t->is_word) {
-                    addr_offset += 2;
-                } else {
-                    addr_offset++;
-                }
-                break;
-            }
-
-            printf("unknown character `%c`\n", *s->cur);
-            exit(1);
-        }
-    }
-}
-
-void resolve_labels(struct token_array *tokens, struct label_array *labels)
-{
-    for (size_t i = 0; i < tokens->size; ++i) {
-        int label_found = 0;
-        struct token *t = tokens->buf + i;
-
-        if (t->kind != TOKEN_LABEL) {
-            continue;
-        }
-
-        for (size_t j = 0; j < labels->size; ++j) {
-            struct label *l = labels->buf + j;
-
-            if (t->lexlen == l->len && memcmp(t->lex, l->start, l->len) == 0) {
-                label_found = 1;
-                t->value = l->addr;
-                break;
-            }
-        }
-
-        if (label_found == 0) {
-            printf("undefined label %.*s\n", (int) t->lexlen, t->lex);
-            exit(1);
-        }
-    }
-}
-
-void write_opcodes(char *pathname, struct token_array *tokens,
-                   struct label_array *labels)
-{
-    FILE *f;
-    uint16_t main_addr;
-    char *main = "_start";
-    int main_found = 0;
-
-    for (size_t i = 0; i < labels->size; ++i) {
-        struct label *l = labels->buf + i;
-
-        if (l->len == strlen(main) && memcmp(main, l->start, l->len) == 0) {
-            main_found = 1;
-            main_addr = l->addr;
-            break;
-        }
-    }
-
-    if (main_found == 0) {
-        printf("%s entry point is missing\n", main);
-        exit(1);
-    }
-
-    f = fopen(pathname, "w");
-    if (f == NULL) {
-        perror("failed to open file");
-        exit(1);
-    }
-
-    fwrite(&main_addr, sizeof(uint16_t), 1, f);
-
-    for (size_t i = 0; i < tokens->size; ++i) {
-        struct token *t = tokens->buf + i;
-        uint8_t inst, lsb, msb;
-
-        switch (t->kind) {
-        case TOKEN_OPCODE:
-            inst = t->is_word << 7 | t->opcode;
-            fwrite(&inst, sizeof(uint8_t), 1, f);
-            break;
-
-        case TOKEN_IMMEDIATE:
-            if (t->is_word == 1) {
-                lsb = t->value;
-                msb = t->value >> 8;
-                fwrite(&lsb, sizeof(uint8_t), 1, f);
-                fwrite(&msb, sizeof(uint8_t), 1, f);
-            } else {
-                inst = t->value;
-                fwrite(&inst, sizeof(uint8_t), 1, f);
-            }
-            break;
-
-        case TOKEN_LABEL:
-            lsb = t->value;
-            msb = t->value >> 8;
-            fwrite(&lsb, sizeof(uint8_t), 1, f);
-            fwrite(&msb, sizeof(uint8_t), 1, f);
-            break;
-        }
-    }
-
-    fclose(f);
+    return isalpha(c) != 0 || c == '_' || c == '.';
 }
 
 int main(int argc, char **argv)
 {
     struct scanner s;
-    struct token_array tokens;
-    struct label_array labels;
-    char *outpath;
+    struct token_array toks;
+    //char *outpath;
 
     argc--;
     argv++;
@@ -373,16 +112,167 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    outpath = create_outpath(*argv, NULL); // LEAK: os is freeing
+    meminit(&toks, sizeof(struct token), 16); // LEAK: os is freeing
+
+    //outpath = create_outpath(*argv, "o"); // LEAK: os is freeing
     s.src = read_file(*argv); // LEAK: os is freeing
     s.cur = s.src;
 
-    meminit(&tokens, sizeof(struct token), 64); // LEAK: os is freeing
-    meminit(&labels, sizeof(struct label), 32); // LEAK: os is freeing
 
-    scan(&s, &tokens, &labels);
-    resolve_labels(&tokens, &labels);
-    write_opcodes(outpath, &tokens, &labels);
+    for (;;) {
+        struct token *t;
+
+        if (*s.cur == '\0') {
+            memgrow(&toks, sizeof(struct token));
+            t = toks.buf + toks.size;
+            toks.size++;
+            t->kind = TOK_EOF;
+            break;
+        }
+
+        s.start = s.cur;
+
+        switch (*s.cur) {
+        case ' ':
+        case '\t':
+        case '\r':
+        case '\n':
+            s.cur++;
+            break;
+
+        case ',':
+            s.cur++;
+
+            memgrow(&toks, sizeof(struct token));
+            t = toks.buf + toks.size;
+            toks.size++;
+
+            t->kind = TOK_COMMA;
+            t->start = s.start;
+            t->len = s.cur - s.start;
+            break;
+
+        case ':':
+            s.cur++;
+
+            memgrow(&toks, sizeof(struct token));
+            t = toks.buf + toks.size;
+            toks.size++;
+
+            t->kind = TOK_COLON;
+            t->start = s.start;
+            t->len = s.cur - s.start;
+            break;
+
+        case ';':
+            while (*s.cur != '\n' && *s.cur != '\0') {
+                s.cur++;
+            }
+            break;
+
+        case '"':
+            s.cur++;
+            while (*s.cur != '"' && *s.cur != '\0') {
+                s.cur++;
+            }
+            if (*s.cur == '\0') {
+                printf("unterminated string literal %.*s\n",
+                       (int) (s.cur - s.start),
+                       s.start);
+                exit(1);
+            }
+            s.cur++;
+
+            memgrow(&toks, sizeof(struct token));
+            t = toks.buf + toks.size;
+            toks.size++;
+
+            t->kind = TOK_STR;
+            t->start = s.start + 1;
+            t->len = s.cur-1 - (s.start + 1);
+            break;
+
+        default:
+            if (isdigit(*s.cur) != 0) {
+                char valbuf[6];
+
+                while (isdigit(*s.cur) != 0) {
+                    s.cur++;
+                }
+
+                memgrow(&toks, sizeof(struct token));
+                t = toks.buf + toks.size;
+                toks.size++;
+
+                t->kind = TOK_NUM;
+                t->start = s.start;
+                t->len = s.cur - s.start;
+
+                if (t->len > sizeof(valbuf) - 1) {
+                    printf("number %.*s is greater than 16bit\n", (int) t->len,
+                                                                  t->start);
+                    exit(1);
+                }
+
+                memset(valbuf, 0, sizeof(valbuf));
+                memcpy(valbuf, t->start, t->len);
+                t->value = atoi(valbuf);
+                if (t->value > (1<<16)) {
+                    printf("number %.*s is greater than 16bit\n", (int) t->len,
+                                                                  t->start);
+                    exit(1);
+                }
+            } else if (ischar(*s.cur) == 1) {
+                while (ischar(*s.cur) == 1 || isdigit(*s.cur) != 0) {
+                    s.cur++;
+                }
+
+                memgrow(&toks, sizeof(struct token));
+                t = toks.buf + toks.size;
+                toks.size++;
+
+                t->start = s.start;
+                t->len = s.cur - s.start;
+                t->kind = TOK_SYMBOL;
+
+                for (size_t i = 0; i < OP_COUNT; ++i) {
+                    char *op = opcodes_str[i];
+
+                    if (t->len == strlen(op) &&
+                            memcmp(op, t->start, t->len) == 0) {
+                        t->kind = TOK_OPCODE;
+                        t->value = i;
+                        break;
+                    }
+                }
+
+                if (t->kind != TOK_SYMBOL) {
+                    break;
+                }
+
+                for (size_t i = 0; i < KWD_COUNT; ++i) {
+                    char *kwd = kwds_str[i];
+
+                    if (t->len == strlen(kwd) &&
+                            memcmp(kwd, t->start, t->len) == 0) {
+                        t->kind = TOK_KWD;
+                        t->value = i;
+                        break;
+                    }
+                }
+            } else {
+                printf("unknown character %c\n", *s.cur);
+                exit(1);
+            }
+            break;
+        }
+    }
+
+    for (size_t i = 0; i < toks.size; ++i) {
+        struct token *t = toks.buf + i;
+
+        printf("kind: %d - lexeme: %.*s - value: %d\n", t->kind, (int) t->len, t->start, t->value);
+    }
 
     return 0;
 }
