b1c85f201a6c5b20f9229733e2f849faa1e9bc31
fosseddy <markfosseddy@gmail.com>
2023-08-15 18:10:51 +0500

    assembler: ready, set, go!

--- build.sh
+++ build.sh
@@ -2,14 +2,24 @@
 
 set -xe
 
-files=vm.c
-outname=vm
+files=
+outname=
 
 flags="-g -Werror=declaration-after-statement -Werror=switch-enum -Werror=switch-default -Wall -Wextra -pedantic -std=c99"
 incl=
 libs=
 
-if [[ $1 = "prod" ]]; then
+if [[ $1 = "vm" ]]; then
+    files=vm.c
+    outname=vm
+fi
+
+if [[ $1 = "asm" ]]; then
+    files=assembler/*.c
+    outname=asm
+fi
+
+if [[ $2 = "prod" ]]; then
     flags=${flags/-g/-O2}
 fi
 
